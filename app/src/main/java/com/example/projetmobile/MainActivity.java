e157804e867f6096ce5ecc838ece6c6079aff27c
Antto <114882531+Antto0509@users.noreply.github.com>
2024-05-27 19:46:54 +0200

    Update MainActivity

--- app/src/main/java/com/example/projetmobile/MainActivity.java
+++ app/src/main/java/com/example/projetmobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.projetmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button PlayButton;
+    private Button ScoreButton;
+    private Button AboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        PlayButton = findViewById(R.id.PlayButton);
+        PlayButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+            startActivity(intent);
+        });
+
+        ScoreButton = findViewById(R.id.ScoreButton);
+        ScoreButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+            startActivity(intent);
+        });
+
+        AboutButton = findViewById(R.id.AboutButton);
+        AboutButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
         });
     }
 }
