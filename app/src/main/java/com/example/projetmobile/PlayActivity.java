b1bd7a526c3086d14a5b2f36f70db784b7da257c
Toinoup80 <127845250+Toinoup80@users.noreply.github.com>
2024-06-10 19:21:12 +0200

    Update activity_play.xml & PlayActivity.java

--- app/src/main/java/com/example/projetmobile/PlayActivity.java
+++ app/src/main/java/com/example/projetmobile/PlayActivity.java
@@ -1,6 +1,8 @@
 package com.example.projetmobile;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PlayActivity extends AppCompatActivity {
+    private TextView textViewCalculation;
+    private TextView textViewResult;
+    private Button button_0;
+    private Button button_1;
+    private Button button_2;
+    private Button button_3;
+    private Button button_4;
+    private Button button_5;
+    private Button button_6;
+    private Button button_7;
+    private Button button_8;
+    private Button button_9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,19 @@ public class PlayActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textViewCalculation = findViewById(R.id.textView_Calculation);
+        textViewResult = findViewById(R.id.textView_Result);
+
+        button_0 = findViewById(R.id.btn_0);
+        button_1 = findViewById(R.id.btn_1);
+        button_2 = findViewById(R.id.btn_2);
+        button_3 = findViewById(R.id.btn_3);
+        button_4 = findViewById(R.id.btn_4);
+        button_5 = findViewById(R.id.btn_5);
+        button_6 = findViewById(R.id.btn_6);
+        button_7 = findViewById(R.id.btn_7);
+        button_8 = findViewById(R.id.btn_8);
+        button_9 = findViewById(R.id.btn_9);
     }
 }
