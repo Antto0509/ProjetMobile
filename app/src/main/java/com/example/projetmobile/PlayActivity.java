7ca38c920541c04d7f8780970923039dab087808
Toinoup80 <127845250+Toinoup80@users.noreply.github.com>
2024-06-11 11:52:16 +0200

    Update activity_play.xml & PlayActivity.java

--- app/src/main/java/com/example/projetmobile/PlayActivity.java
+++ app/src/main/java/com/example/projetmobile/PlayActivity.java
@@ -10,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class PlayActivity extends AppCompatActivity {
     // TextView pour les calculs générés et la saisie
     private TextView textViewCalculation;
@@ -25,10 +27,13 @@ public class PlayActivity extends AppCompatActivity {
     private Button button_7;
     private Button button_8;
     private Button button_9;
-
     // Valeur minimale et maximale générée pour un calcul
     private int min = 1;
     private int max = 100;
+    // Valeurs composant le calcul à résoudre
+    private int firstNumber;
+    private int secondNumber;
+    private String operator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,22 +60,51 @@ public class PlayActivity extends AppCompatActivity {
         button_8 = findViewById(R.id.btn_8);
         button_9 = findViewById(R.id.btn_9);
 
-        button_0.setOnClickListener(view -> pressNumber("0"));
-        button_1.setOnClickListener(view -> pressNumber("1"));
-        button_2.setOnClickListener(view -> pressNumber("2"));
-        button_3.setOnClickListener(view -> pressNumber("3"));
-        button_4.setOnClickListener(view -> pressNumber("4"));
-        button_5.setOnClickListener(view -> pressNumber("5"));
-        button_6.setOnClickListener(view -> pressNumber("6"));
-        button_7.setOnClickListener(view -> pressNumber("7"));
-        button_8.setOnClickListener(view -> pressNumber("8"));
-        button_9.setOnClickListener(view -> pressNumber("9"));
+        button_0.setOnClickListener(view -> addNumber("0"));
+        button_1.setOnClickListener(view -> addNumber("1"));
+        button_2.setOnClickListener(view -> addNumber("2"));
+        button_3.setOnClickListener(view -> addNumber("3"));
+        button_4.setOnClickListener(view -> addNumber("4"));
+        button_5.setOnClickListener(view -> addNumber("5"));
+        button_6.setOnClickListener(view -> addNumber("6"));
+        button_7.setOnClickListener(view -> addNumber("7"));
+        button_8.setOnClickListener(view -> addNumber("8"));
+        button_9.setOnClickListener(view -> addNumber("9"));
+
+        generateCalculation();
     }
 
-    private void pressNumber(String value){
-        addNumber(value);
+    // Générer un nombre aléatoire
+    public int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
     }
+
+    public String getRandomOperator() {
+        String[] operators = {"+", "-", "x", "/"};
+        Random random = new Random();
+        int index = random.nextInt(operators.length);
+        return operators[index];
+    }
+
+    // Affiche le calcul avec les nombres aléatoires générés
+    private void generateCalculation(){
+        firstNumber = getRandomNumber(min, max);
+        secondNumber = getRandomNumber(min, max);
+        operator = getRandomOperator();
+        textViewCalculation.setText(firstNumber + " " + operator + " " + secondNumber);
+    }
+
+    // Ajouter le chiffre au clique sur le clavier numérique
     private void addNumber(String number){
         textViewResult.setText(textViewResult.getText()+number);
     }
+
+    /*
+
+    TRAITER LE RESULTAT DU CALCUL AU DIXIEME PRES
+
+
+     */
+
 }
